094d48c90fa986ece3e66fc65b7cafa49160ec61
Columbia91 <alpha.z91@mail.ru>
2019-06-07 21:38:43 +0600

    Добавьте файлы проекта.

---
+++ SynchronizationLesson/Counter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronizationLesson
+{
+    [Synchronization]
+    public class Counter : ContextBoundObject
+    {
+        private int number = -1;
+        private object lockObject = new object();
+        public void IncreaseNumber()
+        {
+            //lock (lockObject)
+            //{
+            //    while (number <= 20)
+            //    {
+            //        Console.WriteLine($"Поток начал работу ({Thread.CurrentThread.ManagedThreadId})");
+            //        Console.WriteLine(number);
+            //        number++;
+            //        Thread.Sleep(5 * new Random().Next(100));
+            //    }
+            //}
+
+            Monitor.Enter(lockObject);
+            try
+            {
+                while (number <= 20)
+                {
+                    Console.WriteLine($"Поток начал работу ({Thread.CurrentThread.ManagedThreadId})");
+                    Console.WriteLine(number);
+                    number++;
+                    Thread.Sleep(5 * new Random().Next(100));
+                }
+
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+            }
+        }
+    }
+}
---
+++ SynchronizationLesson/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronizationLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Counter();
+            var threads = new Thread[20];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(counter.IncreaseNumber);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
